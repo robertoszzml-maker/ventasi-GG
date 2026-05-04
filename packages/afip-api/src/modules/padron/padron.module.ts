b85d2c5c1fc2d5cc494369dab7df5356af057f65
robertoszzml-maker <robertoszzml@gmail.com>
2026-05-03 23:57:58 -0300

    Fix CertificateService dependency injection in PadronModule

--- packages/afip-api/src/modules/padron/padron.module.ts
+++ packages/afip-api/src/modules/padron/padron.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PadronService } from './padron.service';
 import { PadronController } from './padron.controller';
 import { LoginService } from 'src/services/login/login.service';
+import { CertificateService } from 'src/services/login/certificate.service';
 
 @Module({
   controllers: [PadronController],
-  providers: [PadronService, LoginService],
+  providers: [PadronService, LoginService, CertificateService],
 })
 export class PadronModule { }
