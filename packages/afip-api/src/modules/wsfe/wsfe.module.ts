e9d78b6f1eef50404187ab865b87e2080b3c32fd
robertoszzml-maker <robertoszzml@gmail.com>
2026-05-03 23:51:03 -0300

    Fix CertificateService dependency injection in WsfeModule

--- packages/afip-api/src/modules/wsfe/wsfe.module.ts
+++ packages/afip-api/src/modules/wsfe/wsfe.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WsfeService } from './wsfe.service';
 import { WsfeController } from './wsfe.controller';
 import { LoginService } from 'src/services/login/login.service';
+import { CertificateService } from 'src/services/login/certificate.service';
 
 @Module({
   controllers: [WsfeController],
-  providers: [WsfeService, LoginService],
+  providers: [WsfeService, LoginService, CertificateService],
 })
-export class WsfeModule {}
+export class WsfeModule { }
